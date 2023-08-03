42dcefefa61b2c15065f68e949f4d9e7501134b4
Sameer Mirza <srmirza14@gmail.com>
2023-08-02 22:05:37 -0400

    cleaned some code

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
-// Require express router
 const router = require("express").Router();
 
-// Import all of the API routes
 const apiRoutes = require("./api");
 
-// add prefix of `/api` to all of the api routes
 router.use("/api", apiRoutes);
 
-// 404 Status error message
 router.use((req, res) => {
   res.status(404).send("<h1>ERROR 404 Not Found.</h1>");
 });
 
-// Module exports router
 module.exports = router;
